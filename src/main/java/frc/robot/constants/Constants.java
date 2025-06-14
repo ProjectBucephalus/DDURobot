58e7a30562baf252c3618f4ec6c6b7d4eb771a03
ThePixelatedCat <51038452+Lego-archer-elf@users.noreply.github.com>
2025-06-14 14:43:37 +1000

    algaeRoller done

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -68,8 +68,13 @@ public final class Constants
 
   public static final class Coral
   {
-    
     public static final double forwardSpeed = 0.5;
+    public static final double reverseSpeed = -0.5;
+  }
+
+  public static final class Algae
+  {
+    public static final double forwardspeed = 0.5;
     public static final double reverseSpeed = -0.5;
   }
 }
